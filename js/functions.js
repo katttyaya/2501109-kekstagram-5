c2a6365508beca19a9efa57706b5eef521481380
Екатерина <sapryginakaterina@mail.ru>
2024-10-26 16:54:52 +0500

    functions.js

--- js/functions.js
+++ js/functions.js
@@ -19,3 +19,18 @@ function isPalindsRone(str) {
 isPalindsRone('топот');
 isPalindsRone('ДовОд');
 isPalindsRone('Кекс');
+
+/*5.16. Функции возвращаются*/
+function имяФункции(startWork, endWork, startMeeting, duration) {
+  function timeToMinutes(time) {
+      const [hours, minutes] = time.split(':').map(Number);
+      return hours * 60 + minutes;
+  }
+
+  const startWorkMinutes = timeToMinutes(startWork);
+  const endWorkMinutes = timeToMinutes(endWork);
+  const startMeetingMinutes = timeToMinutes(startMeeting);
+  const endMeetingMinutes = startMeetingMinutes + duration;
+
+  return startMeetingMinutes >= startWorkMinutes && endMeetingMinutes <= endWorkMinutes;
+}
