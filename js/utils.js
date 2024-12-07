c577c8de0933041a23e544e433e95160926383fc
Екатерина <sapryginakaterina@mail.ru>
2024-12-07 23:11:52 +0500

    Add files via upload

---
+++ js/utils.js
@@ -0,0 +1,29 @@
+const getRandomInt = (min, max) => {
+  const lower = Math.ceil(Math.min(min, max));
+  const upper = Math.floor(Math.max(min, max));
+  return Math.floor(Math.random() * (upper - lower + 1) + lower);
+};
+
+const randomIdInRange = (bottom, top) => {
+  const usedIDs = [];
+
+  return function() {
+    let currentInt = getRandomInt(bottom, top);
+    if (usedIDs.length >= (top - bottom + 1)) {
+      return null;
+    }
+    while (usedIDs.includes(currentInt)) {
+      currentInt = getRandomInt(bottom, top);
+    }
+    usedIDs.push(currentInt);
+    return currentInt;
+  };
+};
+
+const randomValueFromArray = (currentArray) => currentArray[getRandomInt(0, currentArray.length - 1)];
+
+export {
+  getRandomInt,
+  randomIdInRange,
+  randomValueFromArray
+};
