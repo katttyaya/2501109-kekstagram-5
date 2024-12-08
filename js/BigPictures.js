685db0b09fc2df823b3d35ceba3971b8e90129c5
Екатерина <sapryginakaterina@mail.ru>
2024-12-08 12:38:15 +0500

    Update BigPictures.js

--- js/BigPictures.js
+++ js/BigPictures.js
@@ -4,15 +4,14 @@ const bigPicture = document.querySelector('.big-picture');
 const bigPictureClosing = bigPicture.querySelector('.big-picture__cancel');
 const bigPicturePhoto = bigPicture.querySelector('.big-picture__img').querySelector('img');
 const bigPictureLikesNumber = bigPicture.querySelector('.likes-count');
-const bigPictureCommentsNumber = bigPicture.querySelector('.comments-count');
 const bigPictureDescr = bigPicture.querySelector('.social__caption');
 const bigPictureAllComments = bigPicture.querySelector('.social__comments');
 const bigPictureCurrentCommentTemplate = bigPictureAllComments.querySelector('.social__comment');
+const bigPictureCommentsLoaderButton = bigPicture.querySelector('.comments-loader');
+const bigPictureCommentsCounter = bigPicture.querySelector('.social__comment-count');
 
 let currentPhoto;
-
-bigPicture.querySelector('.social__comment-count').classList.add('hidden');
-bigPicture.querySelector('.comments-loader').classList.add('hidden');
+let currentCommentIndex = 0;
 
 const makeEmptyComments = () => {
   bigPictureAllComments.innerHTML = '';
@@ -27,16 +26,28 @@ const remakeComment = (comment) => {
   return remadeComment;
 };
 
+const renderComments = () => {
+  let currentIndex = 0;
+  for (let i = currentCommentIndex; i < currentCommentIndex + 5; i++) {
+    if (i === currentPhoto.comments.length) {
+      bigPictureCommentsLoaderButton.classList.add('hidden');
+      currentIndex = i - 1;
+      break;
+    }
+    currentIndex = i;
+    bigPictureAllComments.appendChild(remakeComment(currentPhoto.comments[i]));
+  }
+  currentCommentIndex = currentIndex + 1;
+  bigPictureCommentsCounter.innerHTML = `${currentCommentIndex} из <span class="comments-count">${currentPhoto.comments.length}</span> комментариев`;
+};
+
 const remakePhoto = () => {
   bigPicturePhoto.src = currentPhoto.url;
-  bigPictureCommentsNumber.textContent = currentPhoto.comments.length;
   bigPictureLikesNumber.textContent = currentPhoto.likes;
   bigPictureDescr.textContent = currentPhoto.description;
 
   makeEmptyComments();
-  for (let i = 0; i < currentPhoto.comments.length; i++){
-    bigPictureAllComments.appendChild(remakeComment(currentPhoto.comments[i]));
-  }
+  renderComments(currentPhoto);
 };
 
 const listenKeydown = (evt) => {
@@ -57,11 +68,17 @@ const openBigPost = (currentPost) => {
 function closeBigPost() {
   bigPicture.classList.add('hidden');
   document.body.classList.remove('modal-open');
+  bigPictureCommentsLoaderButton.classList.remove('hidden');
   document.removeEventListener('keydown', listenKeydown);
+  currentCommentIndex = 0;
 }
 
 bigPictureClosing.addEventListener('click', () => {
   closeBigPost();
+});
+
+bigPictureCommentsLoaderButton.addEventListener('click', () => {
+  renderComments();
 });
 
 export {openBigPost};
