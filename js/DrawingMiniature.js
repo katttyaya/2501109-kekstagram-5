cb3350b8646a0e6caa6fa9c2ed174ed50db31af7
Екатерина <sapryginakaterina@mail.ru>
2024-12-08 23:05:35 +0500

    DrawingMiniature.js

--- js/DrawingMiniature.js
+++ js/DrawingMiniature.js
@@ -1,20 +1,60 @@
-const pictureElements = document.querySelector('.pictures');
-const photoTemplate = document.querySelector('#picture').content.querySelector('.picture');
+const checkLength = (string = '', maxLength = 1) => (string.length <= maxLength);
 
-const makeAllPictures = (allPictures) => {
-  const fragmentPictures = document.createDocumentFragment();
-
-  allPictures.forEach(({url, description, likes, comments}) => {
-    const currentPicture = photoTemplate.cloneNode(true);
-    const image = currentPicture.querySelector('.picture__img');
-    image.src = url;
-    image.alt = description;
-    currentPicture.querySelector('.picture__comments').textContent = comments.length;
-    currentPicture.querySelector('.picture__likes').textContent = likes;
-    fragmentPictures.appendChild(currentPicture);
-  });
-
-  pictureElements.appendChild(fragmentPictures);
+const checkPalindrome = function (string = '') {
+  string = string.toLowerCase().replaceAll(' ', '');
+  let result = '';
+  for (let i = string.length - 1; i >= 0; i--) {
+    result += string[i];
+  }
+  return result === string;
 };
 
-export {makeAllPictures};
+
+const findNumbers = function (string) {
+  let result = '';
+  if (!string.isNaN) {
+    string = string.toString();
+  }
+  for (let i = 0; i < string.length; i++) {
+    if (string[i] >= 0 && string[i] <= 9) {
+      result += string[i];
+    }
+  }
+  return parseInt(result.replaceAll(' ', ''), 10);
+};
+
+checkLength();
+checkPalindrome();
+findNumbers();
+
+const normalizeType = function (array) {
+  if (array[0][0] === '0') {
+    array[0] = String(array[0]).substring(1);
+  }
+  if (array[1][0] === '0' && array[1].length === 2) {
+    array[1] = String(array[1]).substring(1);
+  }
+  return array;
+};
+
+const timeToMinutes = function (array) {
+  return (+array[0] * 60 + +array[1]);
+};
+
+const isOutOfBounds = function (workdayStartTime = '', workdayEndTime = '',
+  meetingStartTime = '', meetingDuration = 0) {
+  const workStartTime = timeToMinutes(normalizeType(workdayStartTime.split(':')));
+  const workEndTime = timeToMinutes(normalizeType(workdayEndTime.split(':')));
+  const meetStartTime = timeToMinutes(normalizeType(meetingStartTime.split(':')));
+
+  if (workStartTime > meetStartTime) {
+    return false;
+  } else {
+    if (meetStartTime + meetingDuration <= workEndTime) {
+      return true;
+    }
+  }
+  return false;
+};
+
+isOutOfBounds();
