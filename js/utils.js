2a5d45d0fc0c7cc3c20e7f805807c8aaf6d903c9
Екатерина <sapryginakaterina@mail.ru>
2024-12-09 16:12:36 +0500

    utils.js

--- js/utils.js
+++ js/utils.js
@@ -1,32 +1,28 @@
-const getRandomInt = (min, max) => {
-  const lower = Math.ceil(Math.min(min, max));
-  const upper = Math.floor(Math.max(min, max));
-  return Math.floor(Math.random() * (upper - lower + 1) + lower);
+const getRandomInteger = (a, b) => {
+  const lower = Math.ceil(Math.min(a, b));
+  const upper = Math.floor(Math.max(a, b));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
 };
 
-const randomIdInRange = (bottom, top) => {
-  const usedIDs = [];
+const createRandomNumbers = (min, max) => {
+  const previousValues = [];
 
-  return function() {
-    let currentInt = getRandomInt(bottom, top);
-    if (usedIDs.length >= (top - bottom + 1)) {
+  return function () {
+    let currentValue = getRandomInteger(min, max);
+    if (previousValues.length >= (max - min + 1)) {
       return null;
     }
-    while (usedIDs.includes(currentInt)) {
-      currentInt = getRandomInt(bottom, top);
+    while (previousValues.includes(currentValue)) {
+      currentValue = getRandomInteger(min, max);
     }
-    usedIDs.push(currentInt);
-    return currentInt;
+    previousValues.push(currentValue);
+    return currentValue;
   };
 };
 
-const isEscape = (evt) => evt.key === 'Escape';
+const getRandomArrayElement = (elements) => elements[createRandomNumbers(0, elements.length - 1)()];
 
-const randomValueFromArray = (currentArray) => currentArray[getRandomInt(0, currentArray.length - 1)];
+const isEscapeKey = (evt) => evt.key === 'Escape';
 
-export {
-  getRandomInt,
-  randomIdInRange,
-  randomValueFromArray,
-  isEscape
-};
+export {getRandomInteger, createRandomNumbers, getRandomArrayElement, isEscapeKey};
