57f03e184df07d14f523a00092cfb1dedf8b9dcf
Екатерина <sapryginakaterina@mail.ru>
2024-12-22 22:39:51 +0500

    fullscreen-view.js

--- js/fullscreen-view.js
+++ js/fullscreen-view.js
@@ -1,89 +1,56 @@
-const fullscreenContainer = document.querySelector('.big-picture');
-const containerMeta = fullscreenContainer.querySelector('.big-picture__social');
-const commentsList = containerMeta.querySelector('.social__comments');
-const closeButton = fullscreenContainer.querySelector('.big-picture__cancel');
-const commentsLoader = containerMeta.querySelector('.social__comments-loader');
-const currentCommentsLoaded = containerMeta.querySelector('.current-count');
-const COMMENTS_PER_LOAD = 5;
-let wrapper;
+import { debounce } from './util.js';
 
-const loadComment = (comment) => {
-  commentsList.insertAdjacentHTML('beforeend', `<li class="social__comment"><img class="social__picture" src="${comment.avatar}" alt="${comment.name}" width="35" height="35"><p class="social__text">${comment.message}</p></li>`);
+const PICTURE_COUNT = 25;
+const Filter = {
+  DEFAULT: 'filter-default',
+  RANDOM: 'filter-random',
+  DISCUSSED: 'filter-discussed',
 };
 
-const handleLoaderVisibility = (totalCount) => {
-  currentCommentsLoaded.textContent = commentsList.children.length;
-  if (totalCount === commentsList.children.length) {
-    commentsLoader.classList.add('hidden');
-  } else {
-    commentsLoader.classList.remove('hidden');
+const filterElement = document.querySelector('.img-filters');
+let currentFilter = Filter.DEFAULT;
+let pictures = [];
+
+const sortRandomly = () => Math.random() - 0.5;
+
+const sortByComments = (pictureA, pictureB) =>
+  pictureB.comments.length - pictureA.comments.length;
+
+const getFilteredPictures = () => {
+  switch (currentFilter) {
+    case Filter.RANDOM:
+      return [...pictures].sort(sortRandomly).slice(0, PICTURE_COUNT);
+    case Filter.DISCUSSED:
+      return [...pictures].sort(sortByComments);
+    default:
+      return [...pictures];
   }
 };
 
-const insertComments = (comments) => {
-  commentsList.innerHTML = '';
-
-  for (let i = 0; i < Math.min(comments.length, COMMENTS_PER_LOAD); ++i) {
-    loadComment(comments[i]);
-  }
-  handleLoaderVisibility(comments.length, commentsList.children.length);
-
-  const onLoaderClick = () => {
-    const currentCount = commentsList.children.length;
-    for (let i = currentCount; i < currentCount + Math.min(COMMENTS_PER_LOAD, comments.length - currentCount); ++i) {
-      loadComment(comments[i]);
+const setOnFilterClick = (callback) => {
+  filterElement.addEventListener('click', (evt) => {
+    if (!evt.target.classList.contains('img-filters__button')) {
+      return;
     }
-    handleLoaderVisibility(comments.length, commentsList.children.length);
-  };
 
-  wrapper = onLoaderClick;
+    const clickedButton = evt.target;
+    if (clickedButton.id === currentFilter) {
+      return;
+    }
 
-  commentsLoader.addEventListener('click', wrapper);
+    filterElement
+      .querySelector('.img-filters__button--active')
+      .classList.remove('img-filters__button--active');
+    clickedButton.classList.add('img-filters__button--active');
+    currentFilter = clickedButton.id;
+    callback(getFilteredPictures());
+  });
 };
 
-const drawBigPicture = (url, description, likes, comments) => {
-  if (!url || !comments) {
-    return;
-  }
-  fullscreenContainer.querySelector('.big-picture__img')
-    .querySelector('img').src = url;
-  containerMeta.querySelector('.social__header')
-    .querySelector('.social__likes')
-    .querySelector('.likes-count').textContent = likes;
-  containerMeta.querySelector('.social__header')
-    .querySelector('.social__caption').textContent = description;
-  containerMeta.querySelector('.social__comment-count')
-    .querySelector('.comments-count').textContent = comments.length;
-  insertComments(comments);
+const init = (loadedPictures, callback) => {
+  filterElement.classList.remove('img-filters--inactive');
+  pictures = [...loadedPictures];
+  setOnFilterClick(debounce(callback));
 };
 
-const onDocumentKeydown = (evt) => {
-  if (evt.key === 'Escape') {
-    closeFullview();
-  }
-};
-
-function closeFullview() {
-  fullscreenContainer.classList.add('hidden');
-  document.removeEventListener('keydown', onDocumentKeydown);
-  document.body.classList.remove('modal-open');
-  if (wrapper) {
-    commentsLoader.removeEventListener('click', wrapper);
-    wrapper = null;
-  }
-
-  commentsList.innerHTML = '';
-}
-
-closeButton.addEventListener('click', () => {
-  closeFullview();
-});
-
-function openFullview(url, description, likes, comments) {
-  fullscreenContainer.classList.remove('hidden');
-  document.addEventListener('keydown', onDocumentKeydown);
-  document.body.classList.add('modal-open');
-  drawBigPicture(url, description, likes, comments);
-}
-
-export {openFullview};
+export { init, getFilteredPictures };
