69f5947741682e2aaf502891ff9f8a8de7837d98
Екатерина <sapryginakaterina@mail.ru>
2024-12-22 21:55:54 +0500

    main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,20 @@
-import { getPhotos } from './load.js';
+import { getPhotos} from './load.js';
 import { setFormSubmit } from './form.js';
+import { alertError } from './util.js';
+import { addFilters } from './filters.js';
+import { drawPhotos } from './draw-photos.js';
 
-getPhotos();
+let photos = [];
+
+getPhotos()
+  .then((data) => {
+    drawPhotos(data);
+    photos = data.slice();
+  })
+  .then(() => document.querySelector('.img-filters').classList.remove('img-filters--inactive'))
+  .catch(() => alertError());
+
+addFilters();
 setFormSubmit();
+
+export {photos};
