f2a8063d9ca93c7a1e17cab1f1c9dc39486baead
Екатерина <sapryginakaterina@mail.ru>
2024-12-28 11:23:59 +0500

    main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,36 @@
 import './pictures.js';
+import { loadData, uploadData } from './fetch.js';
+import { renderPhotos } from './pictures.js';
 import './hashtags.js';
 import './slider.js';
+import './filters.js';
+import './photos.js';
+import { showAlert } from './util.js';
+import { showErrorMessage, showSuccessMessage } from './messages.js';
+import { hideModal, setOnFormSubmit } from './hashtags.js';
+
+let photos;
+
+const onSuccess = (data) => {
+  photos = data.slice();
+  renderPhotos(data.slice());
+  document.querySelector('.img-filters').classList.remove('img-filters--inactive');
+};
+
+const onFail = () => {
+  showAlert('Произошла ошибка при загрузке фотографий');
+};
+
+loadData(onSuccess, onFail);
+
+setOnFormSubmit(async (data) => {
+  try {
+    uploadData(data);
+    hideModal();
+    showSuccessMessage();
+  } catch {
+    showErrorMessage();
+  }
+});
+
+export { photos };
